433284613e59f6097ce45e52e0d4e81923719d1d
TenTinh <tinh@ada-soft.com>
2023-10-16 14:45:43 +0700

    Test Commit Git

---
+++ application/modules/common/views/wListData.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Task </title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 200px;
+        padding: 100;
+        flex-direction: column;
+        box-sizing: border-box;
+        justify-content: center;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+        
+
+	}
+    .input-group {
+        position: relative;
+        margin: 20px 0;
+        padding-left: 28px;
+    }
+    .lastname{
+        position: relative; 
+        margin: 20px 0;
+        
+    }
+    .input-chekbox{
+        position: relative; 
+        margin: 20px 60px;
+        padding-left: 28px;
+    }
+
+	</style>
+</head>
+<body>
+
+</body>
+</html>
